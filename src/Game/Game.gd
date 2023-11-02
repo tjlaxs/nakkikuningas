72a51f98ff359679b2004a979b8c03d851ea9f86
Tero Laxström <tlax@neonpeons.com>
2023-11-02 21:16:25 +0200

    Src: Make camera follow player
    Signed-off-by: Tero Laxström <tlax@neonpeons.com>

--- src/Game/Game.gd
+++ src/Game/Game.gd
@@ -10,6 +10,9 @@ const player_definition: EntityDefinition = preload("res://assets/definitions/en
 
 func _ready() -> void:
 	player = Entity.new(Vector2i.ZERO, player_definition)
+	var camera: Camera2D = $Camera2D
+	remove_child(camera)
+	player.add_child(camera)
 	entities.add_child(player)
 	map.generate(player)
 
