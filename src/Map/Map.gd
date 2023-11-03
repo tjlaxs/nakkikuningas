352dc56d3e677072fad2ac1b96a22b15855f76ca
Tero Laxström <tlax@neonpeons.com>
2023-11-03 19:28:11 +0200

    Src: Field of view algorithm
    Signed-off-by: Tero Laxström <tlax@neonpeons.com>

--- src/Map/Map.gd
+++ src/Map/Map.gd
@@ -2,9 +2,11 @@ class_name Map
 extends Node2D
 
 @onready var dungeon_generator: DungeonGenerator = $DungeonGenerator
+@onready var field_of_view: FieldOfView = $FieldOfView
 
 @export var map_width: int = 80
 @export var map_height: int = 45
+@export var fov_radius: int = 8
 
 var map_data: MapData
 
@@ -15,3 +17,6 @@ func generate(player: Entity) -> void:
 func _place_tiles() -> void:
 	for tile in map_data.tiles:
 		add_child(tile)
+
+func update_fov(player_position: Vector2i) -> void:
+	field_of_view.update_fov(map_data, player_position, fov_radius)
